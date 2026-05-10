f01437dfaf8c409c35615950f64d1b9ebe3b8864
Pius Bbaale <mayanjabbaale77@gmail.com>
2026-05-10 12:10:04 +0300

    Implement Masonry Layout for Images

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,11 @@
 import { Minus, Ellipsis } from "lucide-react-native";
-import { Text, View, Image, FlatList, ActivityIndicator } from "react-native";
+import { Text, View, Image, FlatList, ActivityIndicator, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { getImages } from "@/lib/unsplash";
 import { Random } from "unsplash-js/dist/methods/photos/types";
 import { useState, useEffect } from "react";
 import { imageData } from "@/lib/data";
+import ImageCard from "@/components/ImageCard";
 
 export default function Home() {
   const [photos, setPhotos] = useState<Random[]>([]);
@@ -21,7 +22,7 @@ export default function Home() {
   // }, []);
 
   return (
-    <SafeAreaView className="bg-white h-full" edges={['top']}>
+    <SafeAreaView className="flex-1 bg-white mx-1" edges={['top']}>
       <View className="px-4 pt-5 self-start items-center">
         <Text className="font-semibold text-xl">All</Text>
         <View className="-mt-3">
@@ -34,31 +35,27 @@ export default function Home() {
       ) : error ? (
         <Text className="text-red-500 text-center mt-10">{error}</Text>
       ) : (
-        <FlatList
-          data={imageData}
-          numColumns={2}
-          showsVerticalScrollIndicator={false}
-          columnWrapperStyle={{ gap: 4 }}
-          contentContainerClassName='gap-1 mx-1'
-          keyExtractor={(item) => item.id}
-          renderItem={({ item, index }) => {
-            const isEven = index % 2 === 0;
+        <ScrollView showsVerticalScrollIndicator={false} style={{width: '100%'}} >
+          <View className="flex-row">
+            {/* Column 1 */}
+            <View className="flex-1 mr-1">
+              <ImageCard source={imageData[0]?.source} width={imageData[0].width} height={imageData[0].height} />
+              <ImageCard source={imageData[1]?.source} width={imageData[1].width} height={imageData[1].height}/>
+              <ImageCard source={imageData[2]?.source} width={imageData[2].width} height={imageData[2].height}/>
+              <ImageCard source={imageData[4]?.source} width={imageData[4].width} height={imageData[4].height}/>
+              <ImageCard source={imageData[5]?.source} width={imageData[5].width} height={imageData[5].height}/>
+            </View>
 
-            return (
-              <Image
-                // source={{ uri: item.urls.regular }}
-                source={item.source}
-                className="flex-1 h-96 rounded-2xl"
-                // style={{ aspectRatio: 1 }}
-                style={{
-                  width: '100%',
-                  aspectRatio: item.width / item.height, // Uses the dynamic ratio
-                }}
-                resizeMode="cover"
-              />
-            )
-          }}
-        />
+            {/* Column 2 */}
+            <View className="flex-1">
+              <ImageCard source={imageData[6]?.source} width={imageData[6].width} height={imageData[6].height}/>
+              <ImageCard source={imageData[7]?.source} width={imageData[7].width} height={imageData[7].height}/>
+              <ImageCard source={imageData[8]?.source} width={imageData[8].width} height={imageData[8].height}/>
+              <ImageCard source={imageData[9]?.source} width={imageData[9].width} height={imageData[9].height}/>
+              <ImageCard source={imageData[3]?.source} width={imageData[3].width} height={imageData[3].height}/>
+            </View>
+          </View>
+        </ScrollView>
       )}
     </SafeAreaView>
   );
--- components/ImageCard.tsx
+++ components/ImageCard.tsx
@@ -1,12 +1,27 @@
-import { View, Text, Pressable } from 'react-native'
+import { View, Text, Pressable, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { Ellipsis } from 'lucide-react-native';
+import { imageData } from '@/lib/data';
 
-const ImageCard = () => {
-  return (
-    <Pressable>
-        
-    </Pressable>
-  )
+interface ImageCardProps {
+    title?: string,
+    source: any
+    width: number,
+    height: number
+}
+
+const ImageCard = ({title, source, width, height}: ImageCardProps) => {
+    return (
+        <Pressable style={{width: '100%'}} >
+            <Image source={source} className='rounded-2xl h-96' resizeMode='cover' style={{width: '100%', aspectRatio: width/height}} />
+            <View className='flex-row justify-between'>
+                <Text className='font-semibold'>{title}</Text>
+                <TouchableOpacity className='mr-1'>
+                    <Ellipsis size={20} />
+                </TouchableOpacity>
+            </View>
+        </Pressable>
+    )
 }
 
 export default ImageCard
